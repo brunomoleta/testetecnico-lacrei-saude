2defab17dc5362192056c6a0731d12daefa4b10a
Bruno Moleta <brunomoleta@pm.me>
2024-06-18 18:36:13 -0300

    :label: components types

---
+++ src/types/types.d.ts
@@ -0,0 +1,10 @@
+import {ReactNode} from "react";
+
+export interface ChildrenProps {
+  children: ReactNode;
+}
+interface Theme {
+  initialTheme: string;
+}
+
+export type PageTheme = "dark" | "light";
---
+++ src/utils/utils.ts
@@ -0,0 +1,2 @@
+const dateString: string = "2040/01/01";
+export const formattedDate = new Date(dateString);
