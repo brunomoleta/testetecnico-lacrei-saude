2a5082bc8389e60fad26a3d13ca498e43bad430f
Bruno Moleta <brunomoleta@pm.me>
2024-06-18 18:37:59 -0300

    :sparkles: feat:iniciar btn DarkMode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import '@fontsource-variable/nunito';
-import {Providers} from "@/providers/Providers";
+import "@fontsource-variable/nunito";
+import { Providers } from "@/providers/Providers";
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import BaseMain from "@/components/BaseMain";
+import { cookies } from "next/headers";
+import { DARK_COLORS, LIGHT_COLORS } from "@/styled-components/colors.style";
+import DarkLightMode from "@/components/DarkLightMode";
 
 export const metadata: Metadata = {
   title: "Lacrei Saúde",
@@ -16,15 +19,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const savedTheme = cookies().get("color-theme");
+  const theme = savedTheme?.value || "light";
+  const themeColors = theme === "light" ? LIGHT_COLORS : DARK_COLORS;
+
   return (
-    <body>
-    <Providers>
-      <Header>
-        <DarkLightToggle initialTheme={theme}/>
-      </Header>
-      <BaseMain>{children}</BaseMain>
-      <Footer/>
-    </Providers>
-    </body>
+    // @ts-ignore
+    <html lang="pt-BR" data-color-theme={theme} style={themeColors}>
+      <body>
+        <Providers>
+          <Header>
+            <DarkLightMode initialTheme={theme} />
+          </Header>
+          <BaseMain>{children}</BaseMain>
+          <Footer />
+        </Providers>
+      </body>
+    </html>
   );
 }
