20a7b186d22aba8042e9d7a08c37151fb506a6fc
Bruno Moleta <brunomoleta@pm.me>
2024-06-19 18:36:04 -0300

    :construction: wip:header

---
+++ src/components/Header/Header.style.ts
@@ -0,0 +1,13 @@
+"use client";
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  justify-items: end;
+  align-items: center;
+  padding-block-start: 32px;
+  max-width: 1440px;
+  margin: auto;
+  padding-inline:;
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { ChildrenProps } from "@/types/types";
 import Logo from "@/components/Logo";
+import { HeaderWrapper } from "@/components/Header/Header.style";
 
 function Header({ children }: ChildrenProps) {
   return (
     <header>
-      <Logo />
-      {children}
+      <HeaderWrapper>
+        <Logo />
+        {children}
+      </HeaderWrapper>
     </header>
   );
 }
