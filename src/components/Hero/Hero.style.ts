3e24c771842c69ad36fb7e2ad27fb8a77a5df36d
Bruno Moleta <brunomoleta@pm.me>
2024-06-19 18:38:24 -0300

    :recycle: refactor:texto do Hero

---
+++ src/components/Hero/Hero.style.ts
@@ -0,0 +1,8 @@
+"use client";
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding: 40px;
+  max-width: 700px;
+  overflow-wrap: unset;
+`;
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,24 +1,44 @@
+'use client'
+
 import React from "react";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+
 import Balancer from "react-wrap-balancer";
 import Link from "next/link";
+import { Wrapper } from "@/components/Hero/Hero.style";
+import {VisuallyHidden} from "@radix-ui/react-visually-hidden";
 
 function Hero() {
   return (
-    <div>
-        <Balancer>
+    <Wrapper>
+      <VisuallyHidden>
+        <h1>Teste técnico da Lacrei Saúde
+        produzido por Bruno Moleta Santos</h1>
+      </VisuallyHidden>
       <p>
-          Um dos pilares da Lacrei Saúde é a representatividade. Você sabia que
-          existem artistas que fazem arte belíssima apenas com CSS e HTML?
-      </p>
-          <p>
-
-          Uma
-          delas é a <Link href="https://css-art.com/@fossheim">Sarah Fossheim</Link>(elu/delu). Estas são as obras delu que mais
-          gosto, tomara que te agrade também :)
-          </p>
+        <Balancer>
+          Um dos nossos pilares é a representatividade LGBTQIAPN+. Você sabia
+          que existem artistas da comunidade que fazem ilustrações super legais
+          apenas com CSS e HTML?
         </Balancer>
-    </div>
+      </p>
+      <p>
+        <Balancer>
+          Uma delas é a{" "}
+          <Link href="https://css-art.com/@fossheim">Sarah Fossheim</Link>
+          (elu/delu). Estas são as minhas preferidas delu, tomara que tu goste
+          também :)
+        </Balancer>
+      </p>
+      <p>Arraste para os lados para ver as obras :)</p>
+
+      <div>
+        <p>
+          Caso você já tenha visto as artes, sugiro que você vá pra página
+          seguinte que também está bem legal ;)
+        </p>
+        <button>Vamos lá</button>
+      </div>
+    </Wrapper>
   );
 }
 
