8e5e413823c096fc64c73e945a390deaabae96bf
Bruno Moleta <brunomoleta@pm.me>
2024-06-25 19:30:13 -0300

    :construction: wip:Hero

--- src/components/Hero/Hero.style.ts
+++ src/components/Hero/Hero.style.ts
@@ -1,24 +1,18 @@
 "use client";
 import styled from "styled-components";
 import Link from "next/link";
+import Image from "next/image";
 
 export const Wrapper = styled.div`
+  position: relative;
   padding: 40px;
   padding-block-start: 24px;
+    height: 33.6875rem;
 
-  overflow-wrap: unset;
-
-  display: grid;
-  gap: 24px;
-  grid-template-rows: auto auto auto 1fr;
-
-  background-color: var(--hero-background);
+  background: linear-gradient(to right, white 0px, transparent 1200px);
 
   @media (min-width: 62.5rem) {
-    width: 700px;
-    height: 712.5px;
-    margin-inline-end: 24px;
-    margin-block-start: 20px;
+    
   }
 `;
 export const Author = styled(Link)`
@@ -29,9 +23,49 @@ export const Author = styled(Link)`
     text-decoration: unset;
   }
 `;
-export const Instruction = styled.p`
-  display: none;
-  @media (min-width: 62.5rem) {
-    display: unset;
-  }
-`;
+export const ImageWrapper = styled.div`
+    position:absolute;
+    inset: 0;
+    right: 0;
+    opacity: .5;
+    z-index: -1;
+    //filter: grayscale(100%);
+    height: 100%;
+`
+export const MainImage = styled(Image)`
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+`
+
+export const HeroHeading = styled.h2`
+    font-size: 3rem;
+    font-weight: 600;
+    line-height: 3.625rem;
+    color: var(--emerald-60);
+    margin-block: 0 3rem;
+    max-inline-size: 13ch;
+    
+`
+export const HeroParagraph = styled.p`
+    font-size: 1.5rem;
+    line-height: 2.25rem;
+    color: var(--grey-70);
+    
+    max-inline-size: 37ch;
+    margin-block: 0 3rem;    
+`
+
+export const HeroInfo = styled.div`
+    padding-block-start: 120px;
+    max-width: var(--container-max-width);
+    margin: auto;
+    padding-inline: var(--clamp);
+    
+    font-size: 2.25rem;
+`
+
+export const Buttons = styled.div`
+    display: flex;
+    gap: 1.5rem;
+`
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,47 +1,48 @@
-"use client";
-
+import HeroImage from "../../assets/hero.jpg"
 import React from "react";
 
 import Balancer from "react-wrap-balancer";
-import { Author, Instruction, Wrapper } from "@/components/Hero/Hero.style";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
-import { NextPageDesktop } from "@/components/ArtWrapperDefault/Default.style";
-import { Info } from "@/components/ArtGrid/ArtGrid.style";
-import { ArrowRightIcon } from "@radix-ui/react-icons";
+import {
+  Buttons,
+  HeroHeading,
+  HeroInfo,
+  HeroParagraph,
+  ImageWrapper,
+  MainImage,
+  Wrapper
+} from "@/components/Hero/Hero.style";
+import {VisuallyHidden} from "@radix-ui/react-visually-hidden";
+import Button from "@/components/Button";
 
 function Hero() {
   const id = React.useId();
   return (
     <>
       <Wrapper id={`${id}-hero}`}>
+        <ImageWrapper>
+          <MainImage src={HeroImage} layout="fill"  alt={""}/>
+        </ImageWrapper>
         <VisuallyHidden>
           <h1>
-            Teste técnico da Lacrei Saúde produzido por Bruno Moleta Santos
+            Teste técnico da Lacrei Saúde produzido por Bruno Moleta Santos. Junho de 2024.
           </h1>
         </VisuallyHidden>
-        <p>
+        <HeroInfo>
+
+        <HeroHeading>Olá, você está na&nbsp;Lacrei&nbsp;Saúde!</HeroHeading>
+        <HeroParagraph>
           <Balancer>
-            Um dos pilares aqui na Lacrei Saúde é a representatividade
-            LGBTQIAPN+. Você sabia que existem artistas da comunidade que fazem
-            ilustrações super legais apenas com CSS e HTML?
+            Conectamos pessoas LGBTQIAPN+
+            com profissionais de saúde qualificados, proporcionando experiências de cuidado seguras e inclusivas.
           </Balancer>
-        </p>
-        <p>
-          <Balancer>
-            Uma delas é a{" "}
-            <Author href="https://css-art.com/@fossheim">Sarah Fossheim</Author>
-            (elu/delu). Os desenhos a seguir são meus preferidos delu, tomara
-            que tu goste também.
-          </Balancer>
-        </p>
-        <Instruction>Arraste para os lados para ver as obras :)</Instruction>
+        </HeroParagraph>
+        <Buttons>
+          <Button btnType={"primary"}>para pacientes</Button>
+          <Button btnType={"primary"}>para profissionais</Button>
+        </Buttons>
+        </HeroInfo>
       </Wrapper>
-      <Info style={{ paddingInlineEnd: "24px", paddingBlockStart: "10px" }}>
-        <div />
-        <NextPageDesktop href="/pride">
-          Ir para página seguinte <ArrowRightIcon />
-        </NextPageDesktop>
-      </Info>
+
     </>
   );
 }
