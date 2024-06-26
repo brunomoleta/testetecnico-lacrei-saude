8106271f9da08f5792a96a28f08fa0455f04fe14
Bruno Moleta <brunomoleta@pm.me>
2024-06-26 13:07:55 -0300

    :card_file_box: raw:heroData

---
+++ src/data/hero.ts
@@ -0,0 +1,8 @@
+export const hero = {
+  h1:
+    "Teste técnico da Lacrei Saúde produzido por Bruno Moleta Santos.\n" +
+    "            Junho de 2024.",
+  buttonOne: "Para pacientes",
+  buttonTwo: "Para profissionais",
+  heading: "Olá, você está na&nbsp;Lacrei&nbsp;Saúde!",
+};
