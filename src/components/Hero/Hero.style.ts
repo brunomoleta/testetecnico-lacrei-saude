02a3d4af63ffa54ff196af62dbc6fb278df6440f
Bruno Moleta <brunomoleta@pm.me>
2024-06-26 13:09:28 -0300

    :lipstick: style:Hero component

--- src/components/Hero/Hero.style.ts
+++ src/components/Hero/Hero.style.ts
@@ -1,71 +1,65 @@
 "use client";
 import styled from "styled-components";
-import Link from "next/link";
 import Image from "next/image";
 
 export const Wrapper = styled.div`
   position: relative;
   padding: 40px;
   padding-block-start: 24px;
-    height: 33.6875rem;
+  height: 33.6875rem;
 
   background: linear-gradient(to right, white 0px, transparent 1200px);
 
+  margin-block-end: 64px;
+
   @media (min-width: 62.5rem) {
-    
   }
 `;
-export const Author = styled(Link)`
-  color: var(--color-link);
-  text-decoration: underline;
-  padding-inline: 0;
-  &:hover {
-    text-decoration: unset;
-  }
-`;
+
 export const ImageWrapper = styled.div`
-    position:absolute;
-    inset: 0;
-    right: 0;
-    opacity: .5;
-    z-index: -1;
-    //filter: grayscale(100%);
-    height: 100%;
-`
+  position: absolute;
+  inset: 0;
+  right: 0;
+  opacity: 0.5;
+  z-index: -1;
+  height: 100%;
+`;
 export const MainImage = styled(Image)`
-    object-fit: cover;
-    height: 100%;
-    width: 100%;
-`
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+`;
 
 export const HeroHeading = styled.h2`
-    font-size: 3rem;
-    font-weight: 600;
-    line-height: 3.625rem;
-    color: var(--emerald-60);
-    margin-block: 0 3rem;
-    max-inline-size: 13ch;
-    
-`
+  font-size: 3rem;
+  font-weight: 600;
+  line-height: 3.625rem;
+  color: var(--emerald-60);
+  margin-block: 0 3rem;
+  max-inline-size: 13ch;
+`;
 export const HeroParagraph = styled.p`
-    font-size: 1.5rem;
-    line-height: 2.25rem;
-    color: var(--grey-70);
-    
-    max-inline-size: 37ch;
-    margin-block: 0 3rem;    
-`
+  font-size: 1.5rem;
+  line-height: 2.25rem;
+  color: var(--grey-70);
+
+  max-inline-size: 37ch;
+  margin-block: 0 3rem;
+`;
 
 export const HeroInfo = styled.div`
-    padding-block-start: 120px;
-    max-width: var(--container-max-width);
-    margin: auto;
-    padding-inline: var(--clamp);
-    
-    font-size: 2.25rem;
-`
+  padding-block-start: 120px;
+  max-width: var(--container-max-width);
+  margin: auto;
+  padding-inline: var(--clamp);
+
+  font-size: 2.25rem;
+`;
 
 export const Buttons = styled.div`
-    display: flex;
-    gap: 1.5rem;
-`
+  display: flex;
+  gap: 1.5rem;
+`;
+export const Highlight = styled.strong`
+  font-weight: 600;
+`;
