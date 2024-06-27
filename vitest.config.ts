f99d84f62b4b0d471f90d9646bcbf27783d41e57
Bruno Moleta <brunomoleta@pm.me>
2024-06-27 13:11:38 -0300

    :broom: clean:format code

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        environment: 'jsdom',
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
     },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './src')
-        },
-    },
-})
+  },
+});
