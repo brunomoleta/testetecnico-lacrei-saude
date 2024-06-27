474ddc62c2ee2fd67f5c5ea4d2fc33d6a304b5a2
Bruno Moleta <brunomoleta@pm.me>
2024-06-27 18:05:33 -0300

    :recycle: refactor:Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,22 +1,9 @@
 import HeroImage from "../../assets/hero.jpg";
 import React from "react";
 
-import {
-  Buttons,
-  Container,
-  HeroCard,
-  HeroHeading,
-  HeroInfo,
-  Highlight,
-  ImageWrapper,
-  MainImage,
-  Wrapper,
-} from "@/components/Hero/Hero.style";
+import { ImageWrapper, MainImage, Wrapper } from "@/components/Hero/Hero.style";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
-import Button from "@/components/Button";
 import { hero } from "@/data/hero";
-import MainParagraph from "@/components/MainParagraph";
-import HeadingOne from "@/components/Hero/HeadingOne";
 import Info from "@/components/Hero/Info";
 
 function Hero() {
--- src/components/HeroButtons/HeroButtons.tsx
+++ src/components/HeroButtons/HeroButtons.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import Button from "@/components/Button";
 import { hero } from "@/data/hero";
 import { Buttons } from "@/components/Hero/Hero.style";
-import { pessoaLink } from "@/data/headerData";
+import { paciente, profissional } from "@/data/headerData";
 
 function HeroButtons() {
   return (
     <Buttons>
-      <Button href={pessoaLink} btnType={"primary"}>
+      <Button href={paciente} btnType={"primary"}>
         {hero.buttonOne}
       </Button>
-      <Button href={pessoaLink} btnType={"primary"}>
+      <Button href={profissional} btnType={"primary"}>
         {hero.buttonTwo}
       </Button>
     </Buttons>
